08f8bffda6e0e25fabe7aa5829190106495d3592
Alexander C <alexander.culum@gmail.com>
2022-05-16 11:22:22 +0200

    Add initial files.

---
+++ start-scripts/dist/start-local-node.sh
@@ -0,0 +1,5 @@
+export WEB3J_ENDPOINT=http://localhost:7545
+export WEB3J_PRIVATE_KEY=ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+export WEB3J_OPENAPI_HOST=localhost
+export WEB3J_OPENAPI_PORT=9090
+./circles-ubi
